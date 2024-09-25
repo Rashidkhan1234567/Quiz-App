cab4535062884f5ba20752be17a559c6eb3ccfaa
RASHID KHAN <rashidkhaan618@gmail.com>
2024-09-26 01:51:01 +0500

    Done

--- Components/Navbar1.js
+++ Components/Navbar1.js
@@ -16,6 +16,7 @@ function Navbar() {
 
   return (
     <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+      
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto px-4 py-2">
         <a
           className="flex items-center space-x-3 rtl:space-x-reverse"
