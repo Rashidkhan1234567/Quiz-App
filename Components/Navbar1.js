64e8e9608daa4e35e03e2985e714f37ceea571a3
RASHID KHAN <rashidkhaan618@gmail.com>
2024-09-25 22:48:29 +0500

    Done

--- Components/Navbar1.js
+++ Components/Navbar1.js
@@ -31,81 +31,8 @@ function Navbar() {
             M-Rashid Khan
           </span>
         </a>
-        <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
         
-          {/* Hamburger button */}
-          <button
-            onClick={toggleMenu}
-            type="button"
-            className="inline-flex items-center justify-center md:hidden p-2 w-10 h-10 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-            aria-controls="navbar-hamburger"
-            aria-expanded={isOpen ? "true" : "false"}
-          >
-            <span className="sr-only">Open main menu</span>
-            <svg
-              className="w-5 h-5"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 17 14"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M1 1h15M1 7h15M1 13h15"
-              />
-            </svg>
-          </button>
-        </div>
-
-        {/* Mobile menu */}
-        <div
-          className={`${isOpen ? "block" : "hidden"} w-full md:hidden`}
-          id="navbar-hamburger"
-        >
-          <ul className="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
-            <li>
-              <Link
-                href="/"
-                className="block py-2 px-3 text-white bg-blue-700 rounded dark:bg-blue-600"
-                aria-current="page"
-                title="Mouse Move to Object"
-              >
-                MMO
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/mmifs"
-                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                title="Mouse Move to Increase Font Size"
-                >
-                  MMIFS
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/mmcbc"
-                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white"
-                title="Mouse Move to Change Background Color"
-                >
-                  MMCBC
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/qa"
-                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                title="Quiz App"
-                >
-                  QA
-              </Link>
-            </li>
-          </ul>
-        </div>
-
+      
         {/* Desktop menu */}
         <div
           className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
