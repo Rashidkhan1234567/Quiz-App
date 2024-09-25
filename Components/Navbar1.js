e986005c8461b4ca74b37d99d5075e7445713fb2
RASHID KHAN <rashidkhaan618@gmail.com>
2024-09-26 02:03:26 +0500

    Done

--- Components/Navbar1.js
+++ Components/Navbar1.js
@@ -32,11 +32,10 @@ function Navbar() {
             M-Rashid Khan
           </span>
         </a>
-        {/* Desktop menu */}
         <div
-          className="items-center justify-between  w-full md:flex md:w-auto md:order-1"
+          // className="items-center justify-between  w-full md:flex md:w-auto md:order-1"
           id="navbar-sticky"
-          style={{display: flex, justifyContent : "space-between" , alignItems : "center" , width : "100vw", }}
+          style={{display: "flex", justifyContent : "space-between" , alignItems : "center" , width : "100vw", }}
         >
           <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
