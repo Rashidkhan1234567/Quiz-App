32b582648631cea55dae1385addadce3109c286e
RASHID KHAN <rashidkhaan618@gmail.com>
2024-09-26 02:18:28 +0500

    Done

--- Components/Navbar1.js
+++ Components/Navbar1.js
@@ -16,7 +16,6 @@ function Navbar() {
 
   return (
     <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
-      
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto px-4 py-2">
         <a
           className="flex items-center space-x-3 rtl:space-x-reverse"
@@ -32,10 +31,85 @@ function Navbar() {
             M-Rashid Khan
           </span>
         </a>
+        <div className="md:flex md:order-2 space-x-3 lg:hidden md:space-x-0 rtl:space-x-reverse">
+        
+          {/* Hamburger button */}
+          <button
+            onClick={toggleMenu}
+            type="button"
+            className="inline-flex items-center justify-center md:hidden p-2 w-10 h-10 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            aria-controls="navbar-hamburger"
+            aria-expanded={isOpen ? "true" : "false"}
+          >
+            <span className="sr-only">Open main menu</span>
+            <svg
+              className="w-5 h-5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 17 14"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M1 1h15M1 7h15M1 13h15"
+              />
+            </svg>
+          </button>
+        </div>
+
+        {/* Mobile menu */}
         <div
-          // className="items-center justify-between  w-full md:flex md:w-auto md:order-1"
+          className={`${isOpen ? "block" : "hidden"} w-full md:hidden`}
+          id="navbar-hamburger"
+        >
+          <ul className="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+            <li>
+              <Link
+                href="/"
+                className="block py-2 px-3 text-white bg-blue-700 rounded dark:bg-blue-600"
+                aria-current="page"
+                title="Mouse Move to Object"
+              >
+                MMO
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/mmifs"
+                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                title="Mouse Move to Increase Font Size"
+                >
+                  MMIFS
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/mmcbc"
+                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white"
+                title="Mouse Move to Change Background Color"
+                >
+                  MMCBC
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/qa"
+                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                title="Quiz App"
+                >
+                  QA
+              </Link>
+            </li>
+          </ul>
+        </div>
+
+        {/* Desktop menu */}
+        <div
+          className="items-center justify-between  w-full md:flex md:w-auto md:order-1"
           id="navbar-sticky"
-          style={{display: "flex", justifyContent : "space-between" , alignItems : "center" , width : "100vw", }}
         >
           <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
